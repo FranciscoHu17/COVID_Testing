963f0dbd37764f2b9637e4f0bc0bd2ba6b4c65c7
FrHu917 <FrHu917@users.noreply.github.com>
2020-11-23 15:47:07 -0500

    Worked on employee login html

--- server.js
+++ server.js
@@ -24,19 +24,35 @@ con.connect(function(err) {
 */
 /* Express Routing*/
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/main_page.html"));
 });
 
-app.get("/employee", (req,res) => {
+app.get("/employee", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/employee_login.html"));
 });
 
-app.get("/labtech", (req,res) => {
-    //res.sendFile(path.join(__dirname, "/public/labtech_login.html"));
-    res.send("PLACE HOLDER FOR LABTECH");
+app.post("/employee", (req, res) =>{
+    username = req.body.username;
+    password = req.body.password;
+
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+
+    /*checkEmployeeCred(username, password, new function(login){
+        if(login){
+            console.log("LOGGEDIN");
+            //redirect
+        }
+    });*/
 });
 
+app.get("/labtech", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/labtech_login.html"));
+});
 
+checkEmployeeCred = (username, password, callback) => {
+    callback(true);
+}
 port = process.env.PORT || 3000
 app.listen(port, () => { console.log("server started!")});
