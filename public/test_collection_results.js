c68a600a5983733ad49e919ddb454fef0e3df9a7
FrHu917 <FrHu917@users.noreply.github.com>
2020-12-11 02:01:37 -0500

    Finished test collection

--- public/test_collection_results.js
+++ public/test_collection_results.js
@@ -1,43 +1,48 @@
-var i = 1;
-var checkboxHolder = [];
-function addtoTable() {
-	var table = document.getElementById("table");
-	var checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	checkbox.id = "check" + i;
-	checkboxHolder.push(i);
-	var row = document.createElement("tr");
-	var cell1 = document.createElement("td");
-	var cell2 = document.createElement("td");
-	var cell3 = document.createElement("td");
-	cell2.innerHTML = document.getElementById("input1").value;
-	cell3.innerHTML = document.getElementById("input2").value;
-	cell1.append(checkbox);
-	row.appendChild(cell1);
-	row.appendChild(cell2);
-	row.appendChild(cell3);
-	table.appendChild(row);
-	i++;
-	/*var row = table.insertRow(); //can also do table.rows.length
-                var cell1 = row.insertCell(0);
-                var cell2 = row.insertCell(1);
-                cell1.innerHTML = document.getElementById("input1").value;
-				cell2.innerHTML = document.getElementById("input2").value;*/
-				today = new Date();
-				var datetime = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + (today.getDate());
-				datetime += " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-				
-				test = $("input1").val();
-				id = $("input2").val();
-				start = datetime;
-				
-				
-			
-				add_to_db1(test, id, start);
+row = 0;
+
+fetch('/test_collection', {
+    method: 'POST',
+    headers: {
+        'Content-type':'application/json',
+    },
+    body: JSON.stringify({
+        operation: {
+            op: "get"
+        }
+    })
+})
+.then(response => response.json())
+.then(data => {
+    for(i = 0; i < data.length;i++){
+        add_to_test(data[i].employeeID, data[i].testBarcode);
+	}
+})
+.catch((error) => {
+    console.log('Error', error);
+});
+
+add_to_table = () => {
+	employee_id = $(".employee_id").val();
+	test_barcode = $(".test_barcode").val();
+	today = new Date();
+	var datetime = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + (today.getDate());
+	datetime += " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	
+	add_to_db(employee_id, test_barcode, datetime);
+	
 }
 
-add_to_db1 = (test, id, start) => {
-    fetch('/test_collection', {
+add_to_test = (employee, test) => {
+	$(".table").append("<tr>" +
+						   "<td><input type = checkbox class = check"+row+"></td>" +
+						   "<td>" + employee + "</td>" +
+						   "<td>" + test+ "</td>" +
+					   "</tr>");
+	row++;
+}
+
+add_to_db = (employee, test, start) => {
+	fetch('/test_collection', {
         method: 'POST',
         headers: {
             'Content-type':'application/json',
@@ -48,46 +53,53 @@ add_to_db1 = (test, id, start) => {
             },
             test_collect: {
                 testBarcode: test,
-                employeeID: id,
+                employeeID: employee,
                 collectionTime: start
             }
         })
     })
     .then(response => response.json())
     .then(data => {
+		if(data.success){
+			add_to_test(employee, test);
+		}
     })
     .catch((error) => {
         console.log('Error', error);
     });
 }
 
-function deleteF() {
-	var table = document.getElementById("table");
-	if (table.rows.length <= 1) {
-		return;
-	}
-	var bruh = []; //position holder
-	for (var k = 1; k <= checkboxHolder.length; k++) {
-		bruh.push(k);
-	}
-	for (var i = 0; i < checkboxHolder.length; i++) {
-		if (document.getElementById("check" + checkboxHolder[i]).checked) {
-			//actual checkbox array itself
-			delete_from_db1(table.rows[i].cells.item(2).innerHTML, table.rows[i].cells.item(1).innerHTML);
-			table.deleteRow(bruh[i]);
-			for (a = i + 1; a < bruh.length; a++) {
-				bruh[a]--;
-			}
-			bruh.splice(i, 1);
-			//var j = checkboxHolder.indexOf(i);
-			checkboxHolder.splice(i, 1);
-		}
-	}
-	
+find_checked = () => {
+    checked = [];
+    for(i = 0; i < row; i++){
+        if($(".check"+i)[0].checked)
+            checked.push(i);
+    }
+    return checked;
 }
 
-delete_from_db1 = (test, id) => {
-    fetch('/test_collection', {
+fix_tr = () => {
+    for(i = 0; i < row; i++){
+        $(".table").find("tr:eq("+(i+1)+")")
+                          .children()[0].children[0].className = "check"+i;
+    }
+}
+
+delete_checked = () => {
+	checked = find_checked();
+    if(checked.length == 0) return;
+    for(i = 0; i < checked.length; i++){
+        curr = $(".check" + checked[i]).parent().parent();
+		test = curr.children();
+		delete_from_db(test[2].innerHTML);
+        curr.remove();
+    }
+    row -= checked.length;
+    fix_tr();
+}
+
+delete_from_db = (test) => {
+	fetch('/test_collection', {
         method: 'POST',
         headers: {
             'Content-type':'application/json',
@@ -97,8 +109,7 @@ delete_from_db1 = (test, id) => {
                 op: "del"
             },
             test_collect: {
-                testBarcode: test,
-                employeeID: id
+                testBarcode: test
             }
         })
     })
